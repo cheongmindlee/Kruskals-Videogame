2f7e541897b1b57ebbf750b7baac68a3f37ca284
cheongmindlee <cheongmin.dlee@gmail.com>
2025-12-22 20:30:49 -0500

    Add initial city structure and main function

---
+++ kruskals_game/city.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct City{
+    //City name
+    char* name;
+
+    //Coordinate of city
+    int x;
+    int y;
+
+
+} Node;
+
+int main (){
+    
+    return 0;
